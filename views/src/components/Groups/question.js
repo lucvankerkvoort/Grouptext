3ef1b2238d47d151b7a1d670d6539a0744683463
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-06-15 12:58:09 -0700

    changed the question back to the usual look

--- views/src/components/Groups/question.js
+++ views/src/components/Groups/question.js
@@ -3,12 +3,12 @@ import React from "react";
 const Questions = ({ question }) => {
   return (
     <>
-      {/* {(question || []).length < 1 ? null : ( */}
-      <div className="question">
-        <h2>Question</h2>
-        <p>{question || "First initial Question"}</p>
-      </div>
-      {/* )} */}
+      {(question || []).length < 1 ? null : (
+        <div className="question">
+          <h2>Question</h2>
+          <p>{question || "First initial Question"}</p>
+        </div>
+      )}
     </>
   );
 };
